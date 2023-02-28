efbbf6729725eb024303800cf5fc310990785bf2
nvnenasheva <113582210+nvnenasheva@users.noreply.github.com>
2023-02-28 12:13:17 +0100

    fixed bug and prints added

--- get_genome_fraction.py
+++ get_genome_fraction.py
@@ -15,7 +15,7 @@ __status__ = "development"
 parser = argparse.ArgumentParser()
 parser = argparse.ArgumentParser(description='This script reduce the genome of the model organism: take only 25% (--fraction/-f parameter) randomly selected from the genomes.' +
                                              'The program takes a genomic file with sequences as input. The output file contains a set (a fraction of the input set) of randomly' +
-                                             ' selected sequences. The names of the selected sequences are saved to a intermediate file tmp_random_headers.txt')
+                                             ' selected sequences. The names of the selected sequences are saved to a intermediate file fraction_random_headers.txt')
 parser.add_argument('-g', '--genome', required=True, type=str,
                     help='Genome fasta file (possibly softmasked)')
 parser.add_argument('-o', '--output_fasta', required=True, type=str,
@@ -36,6 +36,7 @@ args = parser.parse_args()
 def save_random_headers(random_headers_list, random_headers_file):
     with open(random_headers_file, 'w') as fp:
         rand_headers = sum(random_headers_list, [])
+        print('Total number of selected sequences:', len(rand_headers))
         for item in rand_headers:
             fp.write(item+'\n')
 
@@ -53,8 +54,6 @@ def select_headers_randomly(fasta_inp, random_headers_file, fraction):
     idxs_list = []
 
     last_index = int(subprocess.check_output(['grep', '-c', '>', fasta_inp]).decode('utf-8')) - 1     # save the index of the last sequence from input fasta file
-
-    n = math.ceil(int(last_index + 1)*fraction)                                                       # take only 25% (fraction parameter) randomly selected from the genomes
     print('Input genome.fasta has {} sequences.'.format(last_index + 1))
     print('Select set of contigs, where fraction={}.'.format(fraction))
     print('-----------------------------------------------------------')
@@ -64,6 +63,7 @@ def select_headers_randomly(fasta_inp, random_headers_file, fraction):
         for index, seq_record in enumerate(SeqIO.parse(inp, "fasta")):
             source_name = seq_record.id.split("_")[0]
             idx = seq_record.id.split("_")[1]
+
             if source_name not in source_names:
                 source_names.append(source_name)
                 if idxs:
@@ -72,31 +72,31 @@ def select_headers_randomly(fasta_inp, random_headers_file, fraction):
                     idxs.append(idx)
                 else:
                     idxs.append(idx)
+
             else:
                 if int(index) == last_index:
                     idxs.append(idx)
                     idxs_list.append(idxs)
                 else:
                     idxs.append(idx)
+        #print(source_names, idxs_list)
 
         rand_headers = []
         for i, name in zip(idxs_list, source_names):
-            if n > len(i):
-                n = len(i)
-                rand_idxs = i
-            else:
-                rand_idxs = random.sample(i, n)
-
-            rand_header = [name + '_' + s for s in rand_idxs]
+            n = math.ceil(len(i) * fraction)
+            print('for {} - {} seqs selected'.format(name, n))
+            # select n randomly
+            rand_header = [name + '_' + s for s in random.sample(i, n)]
             rand_headers.append(rand_header)
+            #print(rand_headers)
 
     save_random_headers(random_headers_list=rand_headers,
                         random_headers_file=random_headers_file)
-    print("1. Save randomly selected sequences headers (n={}) to file: ".format(n), random_headers)
+    print("1. Save randomly selected sequences headers to file: ", random_headers)
 
 ''' ******************* END FUNCTIONS *************************************'''
 
-random_headers = 'tmp_random_headers.txt'
+random_headers = 'fraction_random_headers.txt'
 select_headers_randomly(args.genome, random_headers, args.fraction)
 get_genome_fraction(args.genome, random_headers, args.output_fasta)
 print("2. Got small fasta file with randomly selected sequences: ", args.output_fasta)
